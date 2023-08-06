c4bc8db7755ed365725bddf6de81d9a3e82f388d
takuowake <takuowake@gmail.com>
2023-08-06 14:16:47 +0900

    firebase_storageの設定

--- lib/view/start_up/create_account_page.dart
+++ lib/view/start_up/create_account_page.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:twitter_like/utils/authentication.dart';
@@ -28,6 +30,14 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
         image = File(pickedFile.path);
       });
     }
+  }
+
+  Future<void> uploadImage(String uid) async {
+    final FirebaseStorage storageInstance = FirebaseStorage.instance;
+    final Reference ref = storageInstance.ref();
+    await ref.child(uid).putFile(image!);
+    String downloadUrl = await storageInstance.ref(uid).getDownloadURL();
+    print('image_path: $downloadUrl');
   }
 
   @override
@@ -107,7 +117,8 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
                   && passController.text.isNotEmpty
                   && image != null) {
                     var result = await Authentication.signUp(email: emailController.text, pass: passController.text);
-                    if(result == true) {
+                    if(result is UserCredential) {
+                      await uploadImage(result.user!.uid);
                       Navigator.pop(context);
                     }
                   }
