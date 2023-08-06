631640e13910fba1256019123fc181ba2edbb4f8
takuowake <takuowake@gmail.com>
2023-08-06 21:17:19 +0900

    投稿

---
+++ lib/utils/firestore/posts.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:twitter_like/model/post.dart';
+
+class PostFirestore {
+  static final _firestoreInstance = FirebaseFirestore.instance;
+  static final CollectionReference posts = _firestoreInstance.collection('posts');
+
+  static Future<dynamic> addPost(Post newPost) async {
+    try {
+      final CollectionReference _userPosts = _firestoreInstance.collection('users')
+          .doc(newPost.postAccountId).collection('my_posts');
+      var result = await posts.add({
+        'content': newPost.content,
+        'post_account_id': newPost.postAccountId,
+        'created_time': Timestamp.now()
+      });
+      _userPosts.doc(result.id).set({
+        'post_id': result.id,
+        'created_time': Timestamp.now()
+      });
+      print('投稿完了');
+      return true;
+    } on FirebaseException catch(e) {
+      print('投稿エラー: $e');
+      return false;
+    }
+  }
+}
