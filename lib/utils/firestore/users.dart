59be337e185657e34c24c938afbca0b96986d718
takuowake <takuowake@gmail.com>
2023-08-06 15:08:53 +0900

    cloud_firestoreの導入

---
+++ lib/utils/firestore/users.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:twitter_like/model/account.dart';
+import 'package:twitter_like/utils/authentication.dart';
+
+class UserFirestore {
+  static final _firestoreInstance = FirebaseFirestore.instance;
+  static final CollectionReference users = _firestoreInstance.collection('users');
+
+  static Future<dynamic> setUser(Account newAccount) async {
+    try {
+      await users.doc(newAccount.id).set({
+        'name': newAccount.name,
+        'user_id': newAccount.userId,
+        'self_introduction': newAccount.selfIntroduction,
+        'image_path': newAccount.imagePath,
+        'created_time': Timestamp.now(),
+        'updated_time': Timestamp.now(),
+      });
+      print('新規ユーザー作成完了');
+      return true;
+    } on FirebaseException catch(e) {
+      print('新規ユーザー作成エラー: $e');
+      return false;
+    }
+  }
+
+  static Future<dynamic> getUser(String uid) async {
+    try {
+      DocumentSnapshot documentSnapshot = await users.doc(uid).get();
+      Map<String, dynamic> data = documentSnapshot.data() as Map<String, dynamic>;
+      Account myAccount = Account(
+        id: uid,
+        name: data['name'],
+        userId: data['user_id'],
+        selfIntroduction: data['self_introduction'],
+        imagePath: data['image_path'],
+        createdTime: data['created_time'],
+        updatedTime: data['updated_time'],
+      );
+      Authentication.myAccount = myAccount;
+      print('ユーザー取得完了');
+      return true;
+    } on FirebaseException catch(e) {
+      print('ユーザー取得エラー: $e');
+      return false;
+    }
+  }
+}
