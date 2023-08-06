ccbad23ac861a34e371fcea7f178165a939f914b
takuowake <takuowake@gmail.com>
2023-08-06 13:58:49 +0900

    authenticationの設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:twitter_like/view/screen.dart';
 import 'package:twitter_like/view/start_up/login_page.dart';
 import 'package:twitter_like/view/time_line/time_line_page.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
